71073c849cc26b1b4f593af5dcc90eb69b58156c
Lakshya Garg <lakshyarajgarg@gmail.com>
2023-09-04 02:48:16 +0530

    update run api and create models enum

--- ImageTransform/Run.cpp
+++ ImageTransform/Run.cpp
@@ -1,1 +1,16 @@
 #include "Run.h"
+
+Run::Run()
+{
+
+}
+
+Run::~Run()
+{
+
+}
+
+void Run::run(int& model_selected, std::vector<cv::Mat>& image_list,std::unordered_map<std::string, int>& hyper_parameters)
+{
+
+}
--- ImageTransform/Run.h
+++ ImageTransform/Run.h
@@ -1,9 +1,15 @@
 #pragma once
 #include<NvInfer.h>
+#include<opencv2/opencv.hpp>
+
+enum models{BWtoCOLOR,RemoveScratches,MNIST};
 
 class Run
 {
 	public:
+		Run();
+		~Run();
+		void run(int&,std::vector<cv::Mat>&,std::unordered_map<std::string,int>&);
 
 	private:
 
