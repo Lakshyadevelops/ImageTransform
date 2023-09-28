7303e272b623065f236c5b1fc95f0327d11b734e
Lakshya Garg <lakshyarajgarg@gmail.com>
2023-09-29 01:10:23 +0530

    added image display functionality and warning if no source image selected

--- ImageTransform/ImageTransform.cpp
+++ ImageTransform/ImageTransform.cpp
@@ -1,34 +1,66 @@
 #include "ImageTransform.h"
 #include<qfiledialog.h>
+#include<qmessagebox.h>
+#include<qmetaobject.h>
 #include<opencv2/opencv.hpp>
+#include "Run.h"
 
 ImageTransform::ImageTransform(QWidget *parent)
-    : QMainWindow(parent)
+    : QMainWindow(parent), m_model_selected(0)
 {
     ui.setupUi(this);
+    
     connect(ui.pb_single_image, &QPushButton::clicked, this, [this]() {m_single_image_=true; });
     connect(ui.pb_batch_processing, &QPushButton::clicked, this, [this]() {m_single_image_ = false;; });
 
-    connect(ui.pb_single_image, &QPushButton::clicked, this, &ImageTransform::setFilePath);
-    connect(ui.pb_batch_processing, &QPushButton::clicked, this, &ImageTransform::setFilePath);
+    connect(ui.pb_single_image, &QPushButton::clicked, this, &ImageTransform::SetFilePath);
+    connect(ui.pb_batch_processing, &QPushButton::clicked, this, &ImageTransform::SetFilePath);
+
+    connect(ui.pb_start, &QPushButton::clicked, this, &ImageTransform::Start);
+    
+    /* TO DO - e=create enunm and display elements in QComboBox
+    QMetaEnum metaEnum = QMetaEnum::fromType<models>(models);
+    for (int i = 0; i < metaEnum.keyCount(); ++i) {
+        ui.cb_model_select->addItem(metaEnum.key(i));
+    }*/
 
 
 }
 
 ImageTransform::~ImageTransform()
-{}
-
-std::string ImageTransform::getFilePath()
 {
-    return m_FilePath_;
+
 }
 
-void ImageTransform::setFilePath()
+std::string ImageTransform::GetFilePath()
 {
-    m_FilePath_ = QFileDialog::getOpenFileName(nullptr, "Select a File").toStdString();
-    if (m_single_image_) {
-        cv::Mat img = cv::imread(m_FilePath_);
+    return m_file_path_;
+}
 
+void ImageTransform::SetFilePath()
+{
+    if (m_single_image_) {
+        m_file_path_ = QFileDialog::getOpenFileName(nullptr, "Select a File").toStdString();
+        cv::Mat img = cv::imread(m_file_path_);
+
+        cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
+
+        QImage qImage((const unsigned char*)img.data, img.cols, img.rows, QImage::Format_RGB888);
+
+        ui.lb_input_image->setPixmap(QPixmap::fromImage(qImage));
+    }
+    else {
+        m_file_path_ = QFileDialog::getExistingDirectory(nullptr).toStdString();
     }
 }
 
+void ImageTransform::Start() {
+    if (m_file_path_.empty()) {
+        QMessageBox::information(
+            this,
+            tr(""),
+            tr("Select File"));
+    }
+
+
+}
--- ImageTransform/ImageTransform.h
+++ ImageTransform/ImageTransform.h
@@ -2,22 +2,32 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_ImageTransform.h"
+#include<opencv2/opencv.hpp>
+#include"Run.h"
+
 
 class ImageTransform : public QMainWindow
 {
     Q_OBJECT
 
+
 public:
-    ImageTransform(QWidget *parent = nullptr);
+    ImageTransform( QWidget *parent = nullptr );
     ~ImageTransform();
 
-    std::string getFilePath();
-    void setFilePath();
+    //Q_ENUM(models);
+    std::string GetFilePath();
+    void SetFilePath();
+    void Start();
 
 private:
     Ui::ImageTransformClass ui;
-    std::string m_FilePath_;
+    std::string m_file_path_;
     bool m_single_image_;
-
-
+    std::vector< cv::Mat > m_img_list_;
+    int m_model_selected;
+    std::unordered_map< int, std::string > model_names;
+    std::unordered_map< std::string, int > m_hyper_parameters_;
+    Run r_;
+    
 };
